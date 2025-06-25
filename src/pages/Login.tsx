8e7767d3097000967e50d900085228eb2c75b6a5
JowelTisso <joweltisso@gmail.com>
2025-06-25 23:14:56 +0530

    feat : login form ui added

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/login-form";
+
+const Login = () => {
+  return (
+    <div className="h-screen bg-orange-50 flex justify-center">
+      <LoginForm className="justify-center" />
+    </div>
+  );
+};
+
+export default Login;
